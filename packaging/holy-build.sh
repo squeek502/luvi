23de49b4dfabf0043b160c499a4ccabf09af06a8
Ryan Liptak <squeek502@hotmail.com>
2019-03-28 23:15:22 -0700

    Try linux-build-box32-regular

--- packaging/holy-build.sh
+++ packaging/holy-build.sh
@@ -18,11 +18,11 @@ set -x
 
 # Extract and enter source
 # Use /luvi dir to avoid CMake assertion faluire in /
-# mkdir -p luvi
-# tar xzf /io/luvi-src.tar.gz --directory luvi
-# cd luvi
-# make ${BUILD_TYPE}
-# make -j${NPROCS}
-# ldd build/luvi
-# libcheck build/luvi
-# cp build/luvi /io
+mkdir -p luvi
+tar xzf /io/luvi-src.tar.gz --directory luvi
+cd luvi
+make ${BUILD_TYPE}
+make -j${NPROCS}
+ldd build/luvi
+libcheck build/luvi
+cp build/luvi /io
