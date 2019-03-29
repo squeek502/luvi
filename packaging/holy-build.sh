2fc3090cce326a5b92ce70e3b414d1cba742be8b
Ryan Liptak <squeek502@hotmail.com>
2019-03-28 22:54:01 -0700

    Testing of uname -m in holy-build-box-32

--- packaging/holy-build.sh
+++ packaging/holy-build.sh
@@ -7,17 +7,22 @@ echo "Build Type: ${BUILD_TYPE}"
 
 set -e
 
+uname -m
+
 # Activate Holy Build Box environment.
 source /hbb_exe/activate
+
+uname -m
 
 set -x
 
 # Extract and enter source
-mkdir -p luvi
-tar xzf /io/luvi-src.tar.gz --directory luvi
-cd luvi
-make ${BUILD_TYPE}
-make -j${NPROCS}
-ldd build/luvi
-libcheck build/luvi
-cp build/luvi /io
+# Use /luvi dir to avoid CMake assertion faluire in /
+# mkdir -p luvi
+# tar xzf /io/luvi-src.tar.gz --directory luvi
+# cd luvi
+# make ${BUILD_TYPE}
+# make -j${NPROCS}
+# ldd build/luvi
+# libcheck build/luvi
+# cp build/luvi /io
